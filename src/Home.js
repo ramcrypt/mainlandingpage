ed20178392d9b53ddec60237e0d72a9cf2cbc190
ramcrypt <siva.intern@density.exchange>
2023-03-11 16:58:05 +0530

    hero img change

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import { Box, Typography,Button } from '@mui/material';
 import Carousel2 from './Carousel2';
 import React from 'react';
 import ptwo from './components/ptwo.svg';
-import Hero from './components/herofaa.svg';
+import Hero from './components/heroimg.svg';
 /*import Hero from './components/mr.gif';*/
 import "./home.css";
 export default function Home() {
@@ -12,23 +12,23 @@ export default function Home() {
         sx={{
           position:'relative',
           width: '100%',
-          height: 'auto',
+          height: 'inherit',
           background: '#FFFFFF',
         }}
       >
-
-      <div style={{display:'flex',flexDirection:'column',position:'absolute',bottom:'36%',top:'20%',left:'4%',zIndex:1000,mb:100,paddingBottom:'8vw',justifyContent:'center'}}>
+   <div style={{display:'flex',displayDirection:'column',marginLeft:'5vw'}}>
+      <div style={{display:'flex',flexDirection:'column',bottom:'36%',top:'20%',left:'4%',zIndex:1000,mb:100,justifyContent:'center'}}>
       <h1 className="mainc"> Make every Trade count with <span className="mainc" style={{ color: '#4C3CE5' }}>CRYPTO FUTURES</span>
  </h1>
- <div style={{display:'flex',flexDirection:'row'}}>
+ <div style={{display:'flex',flexDirection:'row',paddingBottom:'1vw'}}>
       <p  style={{marginTop:'1.7vw',fontSize: '1.5vw',
       '@media (max-width: 700px)': {
         marginTop:'1.2vw'
     }}}>
         Get 10 USDT as joining bonus
       </p><img  src={ptwo} style={{width:'15%'}}/></div>
-      </div>
-      <div style={{position:'absolute',top:'56%',left:'4%',zIndex:1000}}>
+
+
 
       <Button className="herobutton"
         sx={{
@@ -42,7 +42,7 @@ export default function Home() {
           lineHeight: 18,
 
           padding: "24px 36px",
-          width: '30vw',
+          width: '35vw',
           "&:hover": {
             background: "#E2FF6F"
           },
@@ -60,14 +60,14 @@ export default function Home() {
          '@media (max-width: 700px)': {
           fontSize:'2.5vw'
        }}} >Sign Up Now </Typography>
-      </Button></div>
+      </Button>  </div>
       <div style={{zIndex:0}}>
-        <figure style={{ width: '100%' }}>
+        <figure style={{ width: '50vw' }}>
 
-          <img className="hero-img" src={Hero} alt="Hero image" style={{paddingBottom:0}}/>
+          <img className="hero-img" src={Hero} alt="Hero image" style={{paddingBottom:'10vw',paddingTop:'10vw'}}/>
         </figure></div>
 
-
+</div>
       </Box>
 
 
